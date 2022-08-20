aa71f600d2d64723279811a547eed739aba0edc5
Banyc <36535895+Banyc@users.noreply.github.com>
2022-08-21 00:54:31 +0800

    feat: board entity

--- board_plugin/src/board_plugin.rs
+++ board_plugin/src/board_plugin.rs
@@ -6,6 +6,7 @@ use crate::resources::board_assets::BoardAssets;
 use crate::resources::board_options::BoardOptions;
 use crate::resources::tile::Tile;
 use crate::resources::tile_map::TileMap;
+use crate::systems;
 
 const TILE_SIZE: f32 = 32.0;
 
@@ -20,6 +21,7 @@ impl Plugin for BoardPlugin {
         app.insert_resource(FontPath(self.font_path.clone()));
         app.add_startup_system_to_stage(StartupStage::PreStartup, setup_assets);
         app.add_startup_system(create_board);
+        app.add_system(systems::input::input_handling);
     }
 }
 
@@ -44,15 +46,15 @@ fn create_board(
     };
     log::info!("\n{}", tile_map.tiles_to_string());
 
-    // board
+    // desktop
     {
-        let mut board = commands.spawn();
-        board.insert(Name::new("Board"));
-        board.insert(Transform::default());
-        board.insert(GlobalTransform::default());
-        board.insert(Visibility::default());
-        board.insert(ComputedVisibility::default());
-        board.add_children(|parent| {
+        let mut desktop = commands.spawn();
+        desktop.insert(Name::new("Board"));
+        desktop.insert(Transform::default());
+        desktop.insert(GlobalTransform::default());
+        desktop.insert(Visibility::default());
+        desktop.insert(ComputedVisibility::default());
+        desktop.add_children(|parent| {
             // background
             {
                 let mut background = parent.spawn();
@@ -68,80 +70,92 @@ fn create_board(
                 });
             }
 
-            // tiles
+            // board
             {
-                for y in 0..tile_map.height() {
-                    for x in 0..tile_map.width() {
-                        let coordinates = Coordinates { x, y };
+                let mut board = parent.spawn();
+                board.insert(Name::new("Board"));
+                board.insert_bundle(SpatialBundle {
+                    transform: Transform::from_xyz(
+                        -TILE_SIZE * tile_map.width() as f32 / 2.0,
+                        -TILE_SIZE * tile_map.height() as f32 / 2.0,
+                        1.,
+                    ),
+                    ..Default::default()
+                });
+                board.add_children(|parent| {
+                    // tiles
+                    {
+                        for y in 0..tile_map.height() {
+                            for x in 0..tile_map.width() {
+                                let coordinates = Coordinates { x, y };
 
-                        let mut tile_entity = parent.spawn();
-                        let custom_size = Vec2::splat(TILE_SIZE);
-                        {
-                            let transform = Transform::from_xyz(
-                                x as f32 * TILE_SIZE
-                                    - (TILE_SIZE * tile_map.width() as f32 / 2.0)
-                                    + TILE_SIZE / 2.0,
-                                y as f32 * TILE_SIZE
-                                    - (TILE_SIZE * tile_map.height() as f32 / 2.0)
-                                    + TILE_SIZE / 2.0,
-                                1.0,
-                            );
-                            tile_entity.insert(Name::new(format!("Tile ({})", coordinates)));
-                            tile_entity.insert_bundle(SpriteBundle {
-                                sprite: Sprite {
-                                    color: Color::GRAY,
-                                    custom_size: Some(custom_size),
-                                    ..Default::default()
-                                },
-                                transform,
-                                ..Default::default()
-                            });
-                        }
-
-                        match tile_map.tile(&coordinates) {
-                            Tile::Bomb => {
-                                tile_entity.add_children(|parent| {
-                                    let mut bomb_entity = parent.spawn();
-                                    bomb_entity.insert_bundle(SpriteBundle {
+                                let mut tile_entity = parent.spawn();
+                                let custom_size = Vec2::splat(TILE_SIZE);
+                                {
+                                    let transform = Transform::from_xyz(
+                                        x as f32 * TILE_SIZE + TILE_SIZE / 2.0,
+                                        y as f32 * TILE_SIZE + TILE_SIZE / 2.0,
+                                        1.0,
+                                    );
+                                    tile_entity
+                                        .insert(Name::new(format!("Tile ({})", coordinates)));
+                                    tile_entity.insert_bundle(SpriteBundle {
                                         sprite: Sprite {
-                                            color: Color::RED,
+                                            color: Color::GRAY,
                                             custom_size: Some(custom_size),
                                             ..Default::default()
                                         },
-                                        transform: Transform::from_xyz(0., 0., 1.),
+                                        transform,
                                         ..Default::default()
                                     });
-                                });
-                            }
-                            Tile::BombNeighbor(n) => {
-                                tile_entity.add_children(|parent| {
-                                    let mut bomb_neighbor_entity = parent.spawn();
-                                    bomb_neighbor_entity.insert_bundle(Text2dBundle {
-                                        text: Text {
-                                            sections: vec![TextSection {
-                                                value: n.to_string(),
-                                                style: TextStyle {
-                                                    font: board_assets.font.clone(),
-                                                    font_size: 16.,
-                                                    color: Color::BLACK,
+                                }
+
+                                match tile_map.tile(&coordinates) {
+                                    Tile::Bomb => {
+                                        tile_entity.add_children(|parent| {
+                                            let mut bomb_entity = parent.spawn();
+                                            bomb_entity.insert_bundle(SpriteBundle {
+                                                sprite: Sprite {
+                                                    color: Color::RED,
+                                                    custom_size: Some(custom_size),
                                                     ..Default::default()
                                                 },
-                                            }],
-                                            alignment: TextAlignment {
-                                                vertical: VerticalAlign::Center,
-                                                horizontal: HorizontalAlign::Center,
-                                            },
-                                        },
-                                        transform: Transform::from_xyz(0., 0., 1.),
-                                        ..Default::default()
-                                    });
-                                });
+                                                transform: Transform::from_xyz(0., 0., 1.),
+                                                ..Default::default()
+                                            });
+                                        });
+                                    }
+                                    Tile::BombNeighbor(n) => {
+                                        tile_entity.add_children(|parent| {
+                                            let mut bomb_neighbor_entity = parent.spawn();
+                                            bomb_neighbor_entity.insert_bundle(Text2dBundle {
+                                                text: Text {
+                                                    sections: vec![TextSection {
+                                                        value: n.to_string(),
+                                                        style: TextStyle {
+                                                            font: board_assets.font.clone(),
+                                                            font_size: 16.,
+                                                            color: Color::BLACK,
+                                                            ..Default::default()
+                                                        },
+                                                    }],
+                                                    alignment: TextAlignment {
+                                                        vertical: VerticalAlign::Center,
+                                                        horizontal: HorizontalAlign::Center,
+                                                    },
+                                                },
+                                                transform: Transform::from_xyz(0., 0., 1.),
+                                                ..Default::default()
+                                            });
+                                        });
+                                    }
+                                    Tile::Empty => {}
+                                }
+                                tile_entity.insert(coordinates);
                             }
-                            Tile::Empty => {}
                         }
-                        tile_entity.insert(coordinates);
                     }
-                }
+                });
             }
         });
     }
